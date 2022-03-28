75323c18e2856279b0a9ec8f3f2488c1672bdf58
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-28 19:43:25 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componenets/navbar/navbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
