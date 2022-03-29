98c7ea1c7e0f2db02727bc9f7d017c8fa7d5f8fd
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-29 15:09:16 +0000

    add task componenet

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
+import { TaskService } from 'src/app/services/task.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tasks',
@@ -7,9 +10,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.getTask();
   }
 
+  confirmBox(task: Task) {
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+        this.deleteTask(task.id);
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'error'
+        )
+      }
+    })
+  }
+
+  myTask: Task = {
+    'label': '',
+    'completed': false
+  }
+
+  tasks: Task[] = [];
+
+  resultTasks: Task[] = [];
+
+  editForm: boolean = false;
+
+  showForm: boolean = false;
+
+  searchText: string = '';
+
+  getTask() {
+    this.taskService.findAll()
+      .subscribe(tasks => this.resultTasks = this.tasks = tasks);
+  }
+  deleteTask(id: any) {
+    this.taskService.delete(id).subscribe(() => {
+      this.resultTasks = this.tasks = this.tasks.filter(task => task.id != id);
+    })
+  }
+
+  persistTask() {
+    this.taskService.persist(this.myTask).subscribe((task) => {
+      this.resultTasks = this.tasks = [task, ...this.tasks];
+      this.resteTask();
+      this.showForm = false;
+    })
+  }
+
+  resteTask() {
+    this.myTask = {
+      'label': '',
+      'completed': false
+    }
+  }
+
+  toggleCompleted(task: Task) {
+    this.taskService.completed(task).subscribe(
+      () => {
+        task.completed = !task.completed;
+      }
+    )
+  }
+
+  editeTask(task: Task) {
+    this.myTask = task;
+    this.editForm = true;
+    this.show();
+    // console.log(this.myTask);
+
+  }
+
+  updateTask() {
+    this.taskService.update(this.myTask)
+      .subscribe(
+        task => {
+          this.resteTask();
+          this.editForm = false;
+        }
+      );
+
+  }
+
+  show() {
+    this.showForm = true;
+  }
+
+  searchTasks() {
+    this.resultTasks = this.tasks.filter((task) => task.label.toLowerCase().includes(this.searchText.toLowerCase()));
+  }
 }
